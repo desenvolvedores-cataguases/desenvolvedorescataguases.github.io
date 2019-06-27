a76cec3a3133ceda4b45e1b0633a66a3b5d7d263
luizantoniojr <luizantoniosilvajr@outlook.com>
2019-06-26 23:19:13 -0300

    Gerado o publicavel do portal

--- portal/src/views/Supporters.vue
+++ portal/src/views/Supporters.vue
@@ -11,12 +11,20 @@
           </v-list-tile-action>
         </v-list-tile>
         <v-divider></v-divider>
-          <v-list-tile>
-            <v-list-tile-action>
-              <span class="subheading font-weight-light ma-4">
-                <a href="https://br.freepik.com/fotos-vetores-gratis/negocio">Negócio foto criado por mindandi - br.freepik.com</a>
-              </span>
-            </v-list-tile-action>
+        <v-list-tile>
+          <v-list-tile-action>
+            <span class="subheading font-weight-light ma-4">
+              <a href="https://br.freepik.com/fotos-vetores-gratis/negocio">Negócio foto criado por mindandi - br.freepik.com</a>
+            </span>
+          </v-list-tile-action>
+        </v-list-tile>
+        <v-divider></v-divider>
+        <v-list-tile>
+          <v-list-tile-action>
+            <span class="subheading font-weight-light ma-4">
+              <div>Icons made by <a href="https://www.flaticon.com/authors/adib-sulthon" title="Adib Sulthon">Adib Sulthon</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+            </span>
+          </v-list-tile-action>
         </v-list-tile>
       </v-list>
     </v-layout>
