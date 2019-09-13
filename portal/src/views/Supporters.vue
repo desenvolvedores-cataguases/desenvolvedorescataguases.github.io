b0a3de37176f4cfacd8164c827f80121f0c1d526
luizantoniojr <silvajuniorluizantonio@gmail.com>
2019-09-13 14:22:21 -0300

    Adição de novo suporter.

--- portal/src/views/Supporters.vue
+++ portal/src/views/Supporters.vue
@@ -6,7 +6,24 @@
         <v-list-tile>
           <v-list-tile-action>
             <span class="subheading font-weight-light">
-              Sindicato dos Trabalhadores Indústria Energia Elétrica Cataguases
+              <a href="https://cse.energisa.com.br/">Central de Serviços Energisa</a>
+            </span>
+          </v-list-tile-action>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-action>
+            <span
+              class="subheading font-weight-light"
+            >Sindicato dos Trabalhadores Indústria Energia Elétrica Cataguases</span>
+          </v-list-tile-action>
+        </v-list-tile>
+        <v-divider></v-divider>
+        <v-list-tile>
+          <v-list-tile-action>
+            <span class="subheading font-weight-light">
+              <a
+                href="https://br.freepik.com/fotos-vetores-gratis/negocio"
+              >Negócio foto criado por mindandi - br.freepik.com</a>
             </span>
           </v-list-tile-action>
         </v-list-tile>
@@ -14,15 +31,19 @@
         <v-list-tile>
           <v-list-tile-action>
             <span class="subheading font-weight-light">
-              <a href="https://br.freepik.com/fotos-vetores-gratis/negocio">Negócio foto criado por mindandi - br.freepik.com</a>
-            </span>
-          </v-list-tile-action>
-        </v-list-tile>
-        <v-divider></v-divider>
-        <v-list-tile>
-          <v-list-tile-action>
-            <span class="subheading font-weight-light">
-              <div>Icons made by <a href="https://www.flaticon.com/authors/adib-sulthon" title="Adib Sulthon">Adib Sulthon</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+              <div>
+                Icons made by
+                <a
+                  href="https://www.flaticon.com/authors/adib-sulthon"
+                  title="Adib Sulthon"
+                >Adib Sulthon</a> from
+                <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by
+                <a
+                  href="http://creativecommons.org/licenses/by/3.0/"
+                  title="Creative Commons BY 3.0"
+                  target="_blank"
+                >CC 3.0 BY</a>
+              </div>
             </span>
           </v-list-tile-action>
         </v-list-tile>
